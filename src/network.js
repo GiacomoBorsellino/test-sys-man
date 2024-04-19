a5378d77e4743cb4c108d076e2ff376af171cb79
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2024-04-19 21:48:36 +0200

    trovare prima lettera non ripetuta

--- src/network.js
+++ src/network.js
@@ -52,3 +52,29 @@ function findPath(network) {
     " :)"
   );
 }
+
+/* Trova la prima lettera non ripetuta */
+let str = 'asadsdsex'
+
+let arr = str.split("")
+
+// ['a', 's', 'a', 'd', 's', 's', 'd', 's', 'e', 'x']
+
+for (let i = 0; i < arr.length; i++) {   
+  let lettera = arr[i]
+  let checkLettera = ''
+  for (let j = 0; j < arr.length; j++) {
+    // Non deve essere se stesso
+    if (i !== j) {
+      if (arr[i] === arr[j]) {
+        checkLettera = arr[i]
+        break
+      }
+    }
+  }
+
+  if (checkLettera !== arr[i]) {
+    console.log(arr[i])
+    break
+  }
+}
